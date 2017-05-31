f13486c5e33efb094cccbd9970ee0c8a5affce86
Vaultdweller <vaultdwellerbg13@gmail.com>
2017-05-31 18:11:45 +0300

    Add movement restriction

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,24 +3,37 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-	public float speed = 10;
+	public float speed = 10f;
+	public float padding = 1.5f;
 	
-	private float moveOffset;
+	private float minX;
+	private float maxX;
 
-	// Use this for initialization
 	void Start () {
+		float planeDistance = transform.position.z - Camera.main.transform.position.z;
+		Vector3 leftmost = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, planeDistance));
+		Vector3 rightmost = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, planeDistance));
+		minX = leftmost.x + padding;
+		maxX = rightmost.x - padding;
 	}
 	
-	// Update is called once per frame
 	void Update () {
 			
 		if(Input.GetKey(KeyCode.LeftArrow))
 		{
-			this.transform.position += new Vector3(-speed * Time.deltaTime, 0);
+			transform.position += Vector3.left * speed * Time.deltaTime;
 		}
 		else if (Input.GetKey(KeyCode.RightArrow))
 		{
-			this.transform.position += new Vector3(speed * Time.deltaTime, 0);
+			transform.position += Vector3.right * speed * Time.deltaTime;
 		}
+		
+		RestrictShipPosition();
+	}
+	
+	void RestrictShipPosition()
+	{
+		float clampedX = Mathf.Clamp(transform.position.x, minX, maxX);
+		transform.position = new Vector3(clampedX, transform.position.y);		
 	}
 }
