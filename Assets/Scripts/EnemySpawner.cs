acecf617835da9ac18a14cdbb917ce8227703fdf
Vaultdweller <vaultdwellerbg13@gmail.com>
2017-06-01 20:29:58 +0300

    Add enemy generation

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+
+	public GameObject enemyPrefab;
+
+	void Start () {
+		GameObject enemy = Instantiate(enemyPrefab, new Vector3(0, 0), Quaternion.identity) as GameObject;
+		enemy.transform.parent = transform;
+	}
+}
