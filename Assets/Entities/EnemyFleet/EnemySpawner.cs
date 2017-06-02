b1f91485329fd4e7e3fd55887bdb3cc0ec294250
Vaultdweller <vaultdwellerbg13@gmail.com>
2017-06-02 18:03:07 +0300

    Add enemy positions

---
+++ Assets/Entities/EnemyFleet/EnemySpawner.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+
+	public GameObject enemyPrefab;
+
+	void Start () {
+		foreach (Transform child in transform) {
+			GameObject enemy = Instantiate(enemyPrefab, child.position, Quaternion.identity) as GameObject;
+			print(enemy);
+			enemy.transform.parent = child;			
+		}
+	}
+}
