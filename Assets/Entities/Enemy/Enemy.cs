199c690f8dc8f10e1368d98a678a5c7b1e7faa06
Vaultdweller <vaultdwellerbg13@gmail.com>
2017-06-05 18:45:02 +0300

    Add enemy destruction

---
+++ Assets/Entities/Enemy/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	public float health = 200f;
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		Projectile projectile = col.gameObject.GetComponent<Projectile>();
+		if (projectile != null)
+		{		
+			TakeDamage(projectile.Damage);
+			projectile.Hit();
+			
+		}
+	}
+	
+	void TakeDamage(float damage)
+	{
+		health -= damage;
+		if (health <= 0)
+		{
+			Destroy(gameObject);
+		}		
+	}
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+	public float damage = 100f;
+	
+	public float Damage
+	{
+		get { return damage; }
+	}
+	
+	public void Hit()
+	{
+		Destroy(gameObject);
+	}
+}
