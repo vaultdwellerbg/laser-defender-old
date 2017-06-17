0a2d4ba0612142a154c948c0926a9413b45bdc9e
Vaultdweller <vaultdwellerbg13@gmail.com>
2017-06-17 16:41:47 +0300

    Change bg music and add menu bg

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -4,17 +4,34 @@ using System.Collections;
 public class MusicPlayer : MonoBehaviour {
 
 	static MusicPlayer instance = null;
-
+	private AudioSource music;
+	
+	public AudioClip[] audioClips;
+	
 	void Awake () 
 	{
-		if (instance != null) 
+		if (instance != null && instance != this) 
 		{
 			GameObject.DestroyObject(gameObject);
 		}
 		else
 		{
 			instance = this;
-			GameObject.DontDestroyOnLoad(gameObject);			
+			GameObject.DontDestroyOnLoad(gameObject);
+			music = GetComponent<AudioSource>();	
+			PlayClip(0);	
 		}
+	}
+	
+	void PlayClip(int index)
+	{
+		music.Stop();
+		music.clip = audioClips[index];		
+		music.Play();		
+	}	
+	
+	void OnLevelWasLoaded(int level)
+	{
+		PlayClip(level);
 	}
 }
